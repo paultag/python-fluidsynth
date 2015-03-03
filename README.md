c4ad8166a4b3036e4c78a89796a0ee04caed64ea
Paul Tagliamonte <tag@pault.ag>
2015-03-03 08:46:33 -0500

    Add in meta-information

---
+++ README.md
@@ -0,0 +1,9 @@
+fluidsynth
+==========
+
+
+Python 3 compatable CFFI bindings to Fluidsynth with a Human API
+
+
+
+... eventually. Right now it just plays notes.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+long_description = ""
+
+setup(
+    name="muse",
+    version="0.1",
+    packages=[
+        'fluidsynth',
+    ],
+    author="Paul Tagliamonte",
+    author_email="tag@pault.ag",
+    long_description=long_description,
+    description='n/a',
+    license="Expat",
+    url="http://pault.ag/",
+    platforms=['any']
+)
