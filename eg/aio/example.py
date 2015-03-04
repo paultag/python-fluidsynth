9da96ce2d899f527cdea83954dbfae50a04bcc80
Paul Tagliamonte <tag@pault.ag>
2015-03-03 20:48:22 -0500

    Add in asyncio example (gimick)

---
+++ eg/aio/example.py
@@ -0,0 +1,43 @@
+from muse.chords import (chord, MAJOR, MAJOR_SEVENTH,)
+from muse.tone import Tone
+
+from fluidsynth.aio import AsyncSynth
+import asyncio
+import time
+
+synth = AsyncSynth("/home/tag/piano/Full Grand Piano.sf2", config={
+    "audio.driver": "pulseaudio",
+    "synth.reverb.active": "yes"
+})
+synth.start()
+
+
+
+A3 = Tone(-1200)
+D3 = A3.relative_tone(500)
+F2 = D3.relative_tone(300).relative_tone(-1200)
+
+
+@asyncio.coroutine
+def play_chord(note, chord_, duration, lag):
+    yield from asyncio.sleep(lag)
+    yield from synth.chord(0, [x.to_midi() for x in chord(note, chord_)], 80, duration)
+
+
+@asyncio.coroutine
+def main():
+    yield from asyncio.gather(
+        asyncio.async(play_chord(D3, MAJOR_SEVENTH, 5.0, 0)),
+        asyncio.async(play_chord(A3, MAJOR, 5.0, 2)),
+        asyncio.async(play_chord(A3, MAJOR_SEVENTH, 5.0, 3)),
+        asyncio.async(play_chord(F2, MAJOR_SEVENTH, 5.0, 4.5)),
+    )
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
+
+time.sleep(4)
+
+synth.shutdown()
---
+++ fluidsynth/aio.py
@@ -0,0 +1,36 @@
+# Copyright (c) Paul R. Tagliamonte <tag@pault.ag>, 2015
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import asyncio
+from .api import Synth
+
+
+class AsyncSynth(Synth):
+
+    @asyncio.coroutine
+    def chord(self, channel, keys, velocity, duration):
+        """
+        Play a chord (series of keys) and async sleep.
+        """
+        for key in keys:
+            self.noteon(channel, key, velocity)
+        yield from asyncio.sleep(duration)
+        for key in keys:
+            self.noteoff(channel, key)
