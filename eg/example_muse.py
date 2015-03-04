8ea740e2c7ac1ab3d63b917bbcc622ee18d957b6
Paul Tagliamonte <tag@pault.ag>
2015-03-03 19:58:34 -0500

    Add in play

--- eg/example_muse.py
+++ eg/example_muse.py
@@ -1,7 +1,13 @@
 # Muse, from https://github.com/paultag/python-muse
 
 from muse.scales.minor import NaturalMinorScale
-from muse.chords import chord, MINOR_7TH
+from muse.chords import (
+    chord,
+    MAJOR, MINOR, AUGMENTED, DIMINISHED,
+    DIMINISHED_SEVENTH, HALF_DIMINISHED_SEVENTH,
+    DOMINANT_SEVENTH, MAJOR_SEVENTH, AUGMENTED_SEVENTH,
+    AUGMENTED_MAJOR_SEVENTH,
+)
 from muse.tone import Tone
 
 from fluidsynth import Synth
@@ -28,8 +34,14 @@ def play_chord(chord, duration):
         synth.noteoff(0, c.to_midi())
 
 while True:
-    for note in take(NaturalMinorScale(Tone(0)).acending(), 7):
-        play_chord(chord(note, MINOR_7TH), 2)
+    for prog in [
+        MAJOR, MINOR, AUGMENTED, DIMINISHED,
+        DIMINISHED_SEVENTH, HALF_DIMINISHED_SEVENTH,
+        DOMINANT_SEVENTH, MAJOR_SEVENTH, AUGMENTED_SEVENTH,
+        AUGMENTED_MAJOR_SEVENTH
+    ]:
+        for note in take(NaturalMinorScale(Tone(0)).acending(), 7):
+            play_chord(chord(note, prog), 0.25)
 
 
 synth.shutdown()
